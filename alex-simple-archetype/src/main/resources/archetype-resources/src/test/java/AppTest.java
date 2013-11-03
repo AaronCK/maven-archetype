9d69a7402d49c536dfb64f9e9a16d64b08f60234
AaronCK <Aaron.CKX@gmail.com>
2013-11-03 13:59:03 +0800

    add archetype

---
+++ alex-simple-archetype/src/main/resources/archetype-resources/src/test/java/AppTest.java
@@ -0,0 +1,17 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package};
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+	
+	@Test
+	public void appTest() {
+		System.out.println("Hello Test!");
+	}
+}
---
+++ alex-simple-archetype/target/test-classes/projects/basic/project/basic/src/main/java/it/pkg/App.java
@@ -0,0 +1,8 @@
+package it.pkg;
+
+
+public class App {
+    public static void main( String[] args ) {
+        System.out.println( "Hello World!" );
+    }
+}
